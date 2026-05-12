2646cf1d28ab16fa46284d1ed0993b5bea34e702
bhatvinay7 <bhatvinay74@gmail.com>
2026-05-12 12:06:15 +0530

    update the message type and fix product attachment for normal message

--- src/models/message.models.js
+++ src/models/message.models.js
@@ -19,7 +19,7 @@ const MessageSchema = new mongoose.Schema({
     },
     messageType: {
         type: String,
-        enum: ['text', 'image', 'video', 'file', 'audio', 'location', 'payment_link', 'checkout', 'order_update'],
+        enum: ['text', 'image', 'video', 'file', 'audio', 'location', 'payment_link', 'checkout', 'order_update', 'call', 'contact_seller'],
         default: 'text'
     },
     mediaUrl: String,
