07a5dd20b5eb2d5ab12896ac1188dfb65600f48a
bhatvinay7 <bhatvinay74@gmail.com>
2026-05-20 10:16:42 +0530

    refactor: admin controllers

---
+++ src/controllers/admin/index.js
@@ -0,0 +1,7 @@
+export * from "./auth.controllers.js";
+export * from "./aadhaar.controllers.js";
+export * from "./reports.controllers.js";
+export * from "./users.controllers.js";
+export * from "./business.controllers.js";
+export * from "./analytics.controllers.js";
+export * from "./superadmin.controllers.js";
